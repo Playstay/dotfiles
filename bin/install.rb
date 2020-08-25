6b078fe46c35091ce327de2a368d383ccf931e60
Playstay <ys0701f@gmail.com>
2020-08-26 00:04:08 +0900

    Edit install.rb

--- bin/install.rb
+++ bin/install.rb
@@ -1,13 +1,37 @@
 # frozen_string_literal: true
 
-# /usr/bin/ruby
-
 # installer for dotfiles
 # author: Playstay
 
 require 'optparse'
+require 'pathname'
+require 'fileutils'
 
-opt = Optparser.new
+BASE_DIR = Pathname.pwd.parent
 
-opt.on('-h'){
-}
+def link_files(source,destination,fileutil)
+  source.each_child do |dir|
+    if dir.directory? do
+      link_files(dir, destination,fileutil)
+      continue
+    end
+  end
+end
+opt = OptionParser.new
+
+dest_dir = Pathname(ENV['HOME'])
+
+fileutil = FileUtils
+
+opt.on('-d DIRECTORY','--directory DIRECTORY','install specificied directory') do |dir|
+  dest_dir = Pathname(dir)
+end
+opt.on('-D', '--debug') do  |v|
+  fileutil = FileUtils::DryRun
+end
+
+opt.parse(ARGV)
+
+source_dir = Pathname.pwd.parent
+
+
